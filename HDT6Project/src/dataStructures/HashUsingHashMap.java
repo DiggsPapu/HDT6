f8210fd25164fb432c787da58a5c74e5a12092f2
Your Name <alo20172@uvg.edu.gt>
2022-03-13 11:50:38 -0600

    HashUsingMap insert and search implemented

--- HDT6Project/src/dataStructures/HashUsingHashMap.java
+++ HDT6Project/src/dataStructures/HashUsingHashMap.java
@@ -33,15 +33,37 @@ public class HashUsingHashMap<T> extends Map<T> implements IHashTable<T>  {
 	}
 
 	@Override
-	public T Search(T key) {
+	public String Search(T key, String value) {
 		// TODO Auto-generated method stub
+		// a for to move between the array objects
+		for (int k = 0; k<getDataStructure().get(key).size(); k++) {
+			//If we found the value in the array we return the value and print found
+			if (getDataStructure().get(key).get(k)==value) {
+				System.out.print("Found "+getDataStructure().get(key).get(k)+"\n") ;
+				return getDataStructure().get(key).get(k);
+			}
+		}
+		//If the value is not found we return null and print that was not found
+		System.out.print(value+" was not found\n");
 		return null;
+		
 	}
-
 	@Override
-	public T Remove(T key) {
+	public String Removin(T key, String value) {
 		// TODO Auto-generated method stub
+		//A for to move between the array objects
+		for (int k = 0; k<getDataStructure().get(key).size(); k++) {
+			//If we found the value in the array we remove the value, return the value
+			//and print that it was removed
+			if (getDataStructure().get(key).get(k)==value) {
+				System.out.print(getDataStructure().get(key).get(k)+" was removed\n") ;
+				return getDataStructure().get(key).remove(k);
+			}
+		}
+		//If the value is not found we return null and print that it could not be removed
+		System.out.print(value+" could not be removed\n");
 		return null;
+		
 	}
 	public static void main(String[] args) {
 		HashUsingHashMap<String> EjemploHash =  new HashUsingHashMap<String>();
@@ -50,6 +72,24 @@ public class HashUsingHashMap<T> extends Map<T> implements IHashTable<T>  {
 		EjemploHash.Insert("Comida", "Insectos");
 		for (int k = 0; k<EjemploHash.getDataStructure().get("Carro").size(); k++) {
 			System.out.print(EjemploHash.getDataStructure().get("Carro").get(k)+"\n");
+		}
+		
+		EjemploHash.Search("Carro", "Lalaland");
+		for (int l = 0; l<EjemploHash.getDataStructure().get("Carro").size(); l++) {
+			System.out.print("Object: "+EjemploHash.getDataStructure().get("Carro").get(l)+"\n");
+		}
+		EjemploHash.Search("Carro", "Toyota");
+		for (int n = 0; n<EjemploHash.getDataStructure().get("Carro").size(); n++) {
+			System.out.print("Object: "+EjemploHash.getDataStructure().get("Carro").get(n)+"\n");
+		}
+		
+		EjemploHash.Removin("Carro", "Lalaland");
+		for (int j = 0; j<EjemploHash.getDataStructure().get("Carro").size(); j++) {
+			System.out.print("Object: "+EjemploHash.getDataStructure().get("Carro").get(j)+"\n");
+		}
+		EjemploHash.Removin("Carro", "Toyota");
+		for (int j = 0; j<EjemploHash.getDataStructure().get("Carro").size(); j++) {
+			System.out.print("Object: "+EjemploHash.getDataStructure().get("Carro").get(j)+"\n");
 		}
 	}
 	
--- HDT6Project/src/dataStructures/IHashTable.java
+++ HDT6Project/src/dataStructures/IHashTable.java
@@ -2,6 +2,6 @@ package dataStructures;
 
 public interface IHashTable<T> {
 	public void Insert(T key, T value);
-	public T Search(T key);
-	public T Remove(T key);
+	public String Search(T key, String value);
+	public String Removin(T key, String value);
 }
